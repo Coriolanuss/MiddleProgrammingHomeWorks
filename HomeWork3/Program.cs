ea1a831431e13d0c3a6705985cb13ac4329a31e5
Vadym Penchak <vadimpenchak@gmail.com>
2019-04-18 05:33:51 +0300

    Add Home work 3.

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,197 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    // DISCLAIMER: Sorry for all the mess below, no bright ideas at 4AM :)
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<RoomInfo> firstCollection = RoomInfo.ReadFromCsv(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Data\Barcelona1.csv")).ToList();
+            //List<RoomInfo> secondCollection = RoomInfo.ReadFromCsv(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Data\barcelona2.csv")).ToList();
+
+            // Check if collections are sorted.
+            //Console.WriteLine($"Is first sorted: {IsCollectionSorted(firstCollection)}");
+            //Console.WriteLine($"Is second sorted: {IsCollectionSorted(secondCollection)}");
+
+            // Check if the custom UNION ALL works.
+            //var customUnionAll = HandMadeUnionAll(firstCollection.GetEnumerator(), secondCollection.GetEnumerator()).ToList();
+            //var builtInUnionAll = firstCollection.Concat(secondCollection).OrderBy(x => x.id).ToList();
+            //Console.WriteLine($"Are UNION ALL collections equal: {Enumerable.SequenceEqual(customUnionAll, builtInUnionAll)}");
+
+            // Check if the custom UNION works.
+            //var customUnion = HandMadeUnion(firstCollection.GetEnumerator(), secondCollection.GetEnumerator()).ToList();
+            //var builtInUnion = firstCollection.Union(secondCollection, new RoomInfoEqualityComparer()).OrderBy(x => x.id).ToList();
+            //Console.WriteLine($"Are UNION collections equal: {Enumerable.SequenceEqual(customUnion, builtInUnion)}");
+
+            BenchmarkRunner.Run<Task3_Bechmarks>();
+        }
+
+        private static bool IsCollectionSorted(IEnumerable<RoomInfo> collection) =>
+            Enumerable.SequenceEqual(collection, collection.OrderBy(x => x.id));
+
+        public static IEnumerable<T> HandMadeUnionAll<T>(IEnumerator<T> first, IEnumerator<T> second)
+            where T : IComparable<T>
+        {
+            if (!first.MoveNext())
+            {
+                // enumerate the second to the end, because the first has already ended.
+                while (second.MoveNext())
+                {
+                    yield return second.Current;
+                }
+                yield break;
+            }
+
+            if (!second.MoveNext())
+            {
+                // enumerate the first to the end, because the second has already ended.
+                while (first.MoveNext())
+                {
+                    yield return first.Current;
+                }
+                yield break;
+            }
+
+            while (true)
+            {
+                if (first.Current.CompareTo(second.Current) <= 0) // if the first item is less than or equal to the second item
+                {
+                    yield return first.Current;
+
+                    if (!first.MoveNext())
+                    {
+                        // enumerate the second to the end, because the first has already ended.
+                        yield return second.Current;
+                        while (second.MoveNext())
+                        {
+                            yield return second.Current;
+                        }
+                        break;
+                    }
+                }
+                else // if the first item is greater than the second item
+                {
+                    yield return second.Current;
+
+                    if (!second.MoveNext())
+                    {
+                        // enumerate the first to the end, because the second has already ended.
+                        yield return first.Current;
+                        while (first.MoveNext())
+                        {
+                            yield return first.Current;
+                        }
+                        yield break;
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<T> HandMadeUnion<T>(IEnumerator<T> first, IEnumerator<T> second)
+            where T : IComparable<T>
+        {
+            if (!first.MoveNext())
+            {
+                // enumerate the second to the end, because the first has already ended.
+                while (second.MoveNext())
+                {
+                    yield return second.Current;
+                }
+                yield break;
+            }
+
+            if (!second.MoveNext())
+            {
+                // enumerate the first to the end, because the second has already ended.
+                while (first.MoveNext())
+                {
+                    yield return first.Current;
+                }
+                yield break;
+            }
+
+            while (true)
+            {
+                if (first.Current.CompareTo(second.Current) == 0) // if the first item is equal to the second item
+                {
+                    yield return first.Current;
+
+                    // skip the duplicate
+                    if (!second.MoveNext())
+                    {
+                        // enumerate the first to the end, because the second has already ended.
+                        while (first.MoveNext())
+                        {
+                            yield return first.Current;
+                        }
+                        yield break;
+                    }
+
+                    if (!first.MoveNext())
+                    {
+                        // enumerate the second to the end, because the first has already ended.
+                        while (second.MoveNext())
+                        {
+                            yield return second.Current;
+                        }
+                        break;
+                    }
+                }
+                else if (first.Current.CompareTo(second.Current) < 0) // if the first item is less than the second item
+                {
+                    yield return first.Current;
+
+                    if (!first.MoveNext())
+                    {
+                        // enumerate the second to the end, because the first has already ended.
+                        yield return second.Current;
+                        while (second.MoveNext())
+                        {
+                            yield return second.Current;
+                        }
+                        break;
+                    }
+                }
+                else // if the first item is greater than the second item
+                {
+                    yield return second.Current;
+
+                    if (!second.MoveNext())
+                    {
+                        // enumerate the first to the end, because the second has already ended.
+                        yield return first.Current;
+                        while (first.MoveNext())
+                        {
+                            yield return first.Current;
+                        }
+                        yield break;
+                    }
+                }
+            }
+        }
+    }
+
+    public class Task3_Bechmarks
+    {
+        List<RoomInfo> firstCollection = RoomInfo.ReadFromCsv(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Data\Barcelona1.csv")).ToList();
+        List<RoomInfo> secondCollection = RoomInfo.ReadFromCsv(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Data\barcelona2.csv")).ToList();
+
+        [Benchmark]
+        public void SelectUnionAll() =>
+            Program.HandMadeUnionAll(firstCollection.GetEnumerator(), secondCollection.GetEnumerator()).
+                Select(x => new { x.name, x.latitude, x.longitude }).ToList();
+
+        [Benchmark]
+        public void SelectUnion() =>
+            Program.HandMadeUnion(firstCollection.GetEnumerator(), secondCollection.GetEnumerator()).
+                Select(x => new { x.name, x.latitude, x.longitude }).ToList();
+    }
+}
---
+++ HomeWork3/RoomInfo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork3
+{
+    public class RoomInfo : IComparable<RoomInfo>
+    {
+        public readonly int id;
+        public readonly string name;
+        public readonly int? zipcode;
+        public readonly string smart_location;
+        public readonly string country;
+        public readonly float latitude;
+        public readonly float longitude;
+
+        public RoomInfo(int id, string name, int? zipcode, string smart_location, string country, float latitude, float longitude)
+        {
+            this.id = id;
+            this.name = name;
+            this.zipcode = zipcode;
+            this.smart_location = smart_location;
+            this.country = country;
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+        public static RoomInfo FromCsvRow(string[] csvFields)
+        {
+            return new RoomInfo(
+                id: GetId(idOrUrl: csvFields[0]),
+                name: csvFields[1],
+                zipcode: GetZipcode(csvFields[2]),
+                smart_location: csvFields[3],
+                country: csvFields[4],
+                latitude: Convert.ToSingle(csvFields[5]),
+                longitude: Convert.ToSingle(csvFields[6]));
+        }
+
+        public static IEnumerable<RoomInfo> ReadFromCsv(string filePath)
+        {
+            var parser = new Microsoft.VisualBasic.FileIO.TextFieldParser(filePath)
+            {
+                TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited,
+                Delimiters = new[] { "," },
+                HasFieldsEnclosedInQuotes = true,
+                CommentTokens = new[] { "id", "listing_url" },
+            };
+
+            while (!parser.EndOfData)
+            {
+                string[] row = parser.ReadFields();
+                RoomInfo parsedRoomInfo;
+                try
+                {
+                    parsedRoomInfo = FromCsvRow(row);
+                }
+                catch (FormatException)
+                {
+                    // Source data contains some corrupt rows, so just skip it.
+                    //Console.WriteLine($"Corrupt row: {string.Join(",", row)}");
+                    continue;
+                }
+                yield return parsedRoomInfo;
+            }
+        }
+
+        private static int GetId(string idOrUrl) => Convert.ToInt32(idOrUrl.Substring(idOrUrl.LastIndexOf('/') + 1));
+
+        private static int? GetZipcode(string zipOrEmpty) => string.IsNullOrEmpty(zipOrEmpty) ? (int?)null : Convert.ToInt32(zipOrEmpty);
+
+        public int CompareTo(RoomInfo other) => id.CompareTo(other.id);
+    }
+
+    public class RoomInfoEqualityComparer : IEqualityComparer<RoomInfo>
+    {
+        public bool Equals(RoomInfo x, RoomInfo y) => x.id == y.id;
+
+        public int GetHashCode(RoomInfo obj) => obj.id;
+    }
+}
